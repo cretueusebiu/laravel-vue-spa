58b4728d82ee66a5d87df46390c2dd3804b37716
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-01-17 17:12:43 +0200

    Add dashboard card

--- resources/assets/js/pages/home.vue
+++ resources/assets/js/pages/home.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Home</h1>
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <div class="card">
+        <div class="card-header">Dashboard</div>
+        <div class="card-block">
+          You are logged in!
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
