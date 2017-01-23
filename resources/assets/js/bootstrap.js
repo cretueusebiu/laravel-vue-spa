aef7802c0ce83370b63781b64d354518eaab422d
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-01-23 16:21:59 +0200

    Add bootstrap js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import $ from 'jquery'
+import Tether from 'tether'
 import Meta from 'vue-meta'
 import Router from 'vue-router'
 import './utils/interceptors'
@@ -6,6 +8,10 @@ import { HasError4, AlertError, AlertSuccess } from 'vform'
 
 Vue.use(Router)
 Vue.use(Meta)
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
 
 // Register components
 Vue.component('has-error', HasError4)
