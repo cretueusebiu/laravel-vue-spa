3e8403210b8f050accd446693ffbc960f7f5b323
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-07-17 11:32:00 +0300

    Delete build foder

--- build/dev-client.js
+++
@@ -1,8 +0,0 @@
-require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true&path=http://localhost:8080/__webpack_hmr')
-
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-})
