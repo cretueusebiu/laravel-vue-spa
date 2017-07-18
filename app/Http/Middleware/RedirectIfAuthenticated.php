2d712257f56fc7a39ccc5009a262584a76c3d4f4
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-07-18 09:39:31 +0300

    Return json

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return response()->json(['error' => 'Already authenticated.'], 400);
         }
 
         return $next($request);
