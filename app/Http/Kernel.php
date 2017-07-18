24e1ce563377ac26322c3ba0296dd9b9ccedd8ff
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-07-18 09:45:07 +0300

    Remove middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,7 +50,6 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth.api' => \App\Http\Middleware\AuthenticateApi::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
