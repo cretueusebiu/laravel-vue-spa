97250c148772d049c1c566f2399474e52e193a71
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-09-02 22:35:31 +0300

    Set default string length for testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->runningUnitTests()) {
+            Schema::defaultStringLength(191);
+        }
     }
 
     /**
