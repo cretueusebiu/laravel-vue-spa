4d6dbb61fae87b07849c30ac4a28ebabdbaa0adb
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-12-14 13:53:06 +0200

    Add locale middleware

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($locale = $this->parseLocale($request)) {
+            app()->setLocale($locale);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * @param  \Illuminate\Http\Request $request
+     * @return string|null
+     */
+    protected function parseLocale($request)
+    {
+        $locales = config('app.locales');
+
+        // if (array_key_exists($locale = $request->cookie('locale'), $locales)) {
+        //     return $locale;
+        // }
+
+        $locale = $request->server('HTTP_ACCEPT_LANGUAGE');
+        $locale = substr($locale, 0, strpos($locale, ',') ?: strlen($locale));
+
+        if (array_key_exists($locale, $locales)) {
+            return $locale;
+        }
+
+        if (array_key_exists($locale, $locales)) {
+            return $locale;
+        }
+
+        $locale = substr($locale, 0, 2);
+        if (array_key_exists($locale, $locales)) {
+            return $locale;
+        }
+    }
+}
--- tests/Feature/LocaleTest.php
+++ tests/Feature/LocaleTest.php
@@ -18,7 +18,17 @@ class LocaleTest extends TestCase
     function set_locale_from_header()
     {
         $this->withHeaders(['Accept-Language' => 'zh-CN'])
-            ->postJson('/api/login')
-            ->dump();
+            ->postJson('/api/login');
+
+        $this->assertEquals('zh-CN', $this->app->getLocale());
+    }
+
+    /** @test */
+    function set_locale_from_header_short()
+    {
+        $this->withHeaders(['Accept-Language' => 'en-US'])
+            ->postJson('/api/login');
+
+        $this->assertEquals('en', $this->app->getLocale());
     }
 }
