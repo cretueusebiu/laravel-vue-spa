e35a7afbb60d660091c95224cd694a1f74fafd1f
curder <curder@foxmail.com>
2017-12-14 16:09:47 +0800

    add Chinese Language supports.

---
+++ resources/lang/zh_CN/auth.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => '用户名或手机号与密码不匹配或用户被禁用',
+    'throttle' => '失败次数太多，请在:seconds秒后再尝试',
+];
