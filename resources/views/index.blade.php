0f5256867daf7150ab6a681457746a14c6aec6cb
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-16 20:24:14 +0200

    Merge views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,24 @@
+@php
+$config = [
+    'appName' => config('app.name'),
+    'locale' => $locale = app()->getLocale(),
+    'locales' => config('app.locales'),
+    'githubAuth' => config('services.github.client_id'),
+    'translations' => json_decode(file_get_contents(resource_path("lang/{$locale}.json")), true),
+];
+
+$polyfills = [
+    'Promise',
+    'Object.assign',
+    'Object.values',
+    'Array.prototype.find',
+    'Array.prototype.findIndex',
+    'Array.prototype.includes',
+    'String.prototype.includes',
+    'String.prototype.startsWith',
+    'String.prototype.endsWith',
+];
+@endphp
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
@@ -11,6 +32,19 @@
 <body>
   <div id="app"></div>
 
-  @include('scripts')
+  {{-- Global configuration object --}}
+  <script>window.config = @json($config);</script>
+
+  {{-- Polyfill JS features via polyfill.io --}}
+  <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features={{ implode(',', $polyfills) }}"></script>
+
+  {{-- Load the application scripts --}}
+  @if (app()->isLocal())
+    <script src="{{ mix('js/app.js') }}"></script>
+  @else
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+  @endif
 </body>
 </html>
