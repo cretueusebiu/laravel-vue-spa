4202f2c2611489b2f0d2d690a452e4878568ac63
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-16 17:55:09 +0200

    Use window

--- resources/assets/js/components/global/LoginWithGithub.vue
+++ resources/assets/js/components/global/LoginWithGithub.vue
@@ -60,10 +60,10 @@ function openWindow (url, title, options = {}) {
 
   options = { url, title, width: 600, height: 720, ...options }
 
-  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left
-  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top
-  const width = window.innerWidth || document.documentElement.clientWidth || screen.width
-  const height = window.innerHeight || document.documentElement.clientHeight || screen.height
+  const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screen.left
+  const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screen.top
+  const width = window.innerWidth || document.documentElement.clientWidth || window.screen.width
+  const height = window.innerHeight || document.documentElement.clientHeight || window.screen.height
 
   options.left = ((width / 2) - (options.width / 2)) + dualScreenLeft
   options.top = ((height / 2) - (options.height / 2)) + dualScreenTop
