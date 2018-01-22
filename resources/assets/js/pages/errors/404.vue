ee0b2a5703b2d7d05797289b40e28c1b18c9844d
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-22 22:10:51 +0200

    Use card for error page

--- resources/assets/js/pages/errors/404.vue
+++ resources/assets/js/pages/errors/404.vue
@@ -1,14 +1,14 @@
 <template>
   <main-layout>
-    <div class="text-center">
-      <h2 class="mb-4">{{ $t('page_not_found') }}</h2>
+    <card class="text-center">
+      <h3 class="mb-4">{{ $t('page_not_found') }}</h3>
 
       <div class="links">
         <router-link :to="{ name: 'welcome' }">
           {{ $t('go_home') }}
         </router-link>
       </div>
-    </div>
+    </card>
   </main-layout>
 </template>
 
