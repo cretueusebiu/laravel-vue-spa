e8bb8d5e12303ad9e47a6a44ef7895babddc9c34
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-24 11:43:19 +0200

    Clean up routes

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -1,21 +1,20 @@
-const Home = () => import('~/pages/home').then(m => m.default || m)
-const Welcome = () => import('~/pages/welcome').then(m => m.default || m)
+const Welcome = () => import('~/pages/welcome')
+const Login = () => import('~/pages/auth/login')
+const Register = () => import('~/pages/auth/register')
+const PasswordEmail = () => import('~/pages/auth/password/email')
+const PasswordReset = () => import('~/pages/auth/password/reset')
 
-const Login = () => import('~/pages/auth/login').then(m => m.default || m)
-const Register = () => import('~/pages/auth/register').then(m => m.default || m)
-const PasswordReset = () => import('~/pages/auth/password/reset').then(m => m.default || m)
-const PasswordRequest = () => import('~/pages/auth/password/email').then(m => m.default || m)
-
-const Settings = () => import('~/pages/settings/index').then(m => m.default || m)
-const SettingsProfile = () => import('~/pages/settings/profile').then(m => m.default || m)
-const SettingsPassword = () => import('~/pages/settings/password').then(m => m.default || m)
+const Home = () => import('~/pages/home')
+const Settings = () => import('~/pages/settings/index')
+const SettingsProfile = () => import('~/pages/settings/profile')
+const SettingsPassword = () => import('~/pages/settings/password')
 
 export default [
   { path: '/', name: 'welcome', component: Welcome },
 
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: Register },
-  { path: '/password/reset', name: 'password.request', component: PasswordRequest },
+  { path: '/password/reset', name: 'password.request', component: PasswordEmail },
   { path: '/password/reset/:token', name: 'password.reset', component: PasswordReset },
 
   { path: '/home', name: 'home', component: Home },
@@ -25,5 +24,5 @@ export default [
     { path: 'password', name: 'settings.password', component: SettingsPassword }
   ] },
 
-  { path: '*', component: require('~/pages/errors/404.vue') }
+  { path: '*', component: require('~/pages/errors/404') }
 ]
