81bd5456f0333b2efbd17d8e475b681ee566637b
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2019-05-05 19:19:58 +0300

    Refactor routes

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,28 +1,23 @@
-// Helper function for lazy loading routes using Webpack dynamic imports
-// See: https://router.vuejs.org/guide/advanced/lazy-loading.html#grouping-components-in-the-same-chunk
-// See: https://webpack.js.org/guides/code-splitting/
-function page (name) {
-  return () => import(
-    /* webpackChunkName: 'view-' */ `~/pages/${name}.vue`
-  ).then(module => module.default || module)
+function page (path) {
+  return () => import(/* webpackChunkName: '' */ `~/pages/${path}`).then(m => m.default || m)
 }
 
 export default [
-  { path: '/', name: 'welcome', component: page('welcome') },
+  { path: '/', name: 'welcome', component: page('welcome.vue') },
 
-  { path: '/login', name: 'login', component: page('auth/login') },
-  { path: '/register', name: 'register', component: page('auth/register') },
-  { path: '/password/reset', name: 'password.request', component: page('auth/password/email') },
-  { path: '/password/reset/:token', name: 'password.reset', component: page('auth/password/reset') },
+  { path: '/login', name: 'login', component: page('auth/login.vue') },
+  { path: '/register', name: 'register', component: page('auth/register.vue') },
+  { path: '/password/reset', name: 'password.request', component: page('auth/password/email.vue') },
+  { path: '/password/reset/:token', name: 'password.reset', component: page('auth/password/reset.vue') },
 
-  { path: '/home', name: 'home', component: page('home') },
+  { path: '/home', name: 'home', component: page('home.vue') },
   { path: '/settings',
-    component: page('settings/index'),
+    component: page('settings/index.vue'),
     children: [
       { path: '', redirect: { name: 'settings.profile' } },
-      { path: 'profile', name: 'settings.profile', component: page('settings/profile') },
-      { path: 'password', name: 'settings.password', component: page('settings/password') }
+      { path: 'profile', name: 'settings.profile', component: page('settings/profile.vue') },
+      { path: 'password', name: 'settings.password', component: page('settings/password.vue') }
     ] },
 
-  { path: '*', component: page('errors/404') }
+  { path: '*', component: page('errors/404.vue') }
 ]
