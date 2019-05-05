0311e73d18e2360ef2b1ccc0d258b4f30fa09711
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2019-05-05 19:20:18 +0300

    Use default chunkname

--- resources/js/plugins/i18n.js
+++ resources/js/plugins/i18n.js
@@ -14,7 +14,7 @@ const i18n = new VueI18n({
  */
 export async function loadMessages (locale) {
   if (Object.keys(i18n.getLocaleMessage(locale)).length === 0) {
-    const messages = await import(/* webpackChunkName: "lang-[request]" */ `~/lang/${locale}`)
+    const messages = await import(/* webpackChunkName: '' */ `~/lang/${locale}`)
     i18n.setLocaleMessage(locale, messages)
   }
 
