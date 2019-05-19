ecb9d523fd14cd7fca270f5d954e3e83ca672150
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2019-05-19 11:29:52 +0300

    Update welcome.vue

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -22,11 +22,7 @@
       </div>
 
       <div class="links">
-        <a href="https://laravel.com/docs">Documentation</a>
-        <a href="https://laracasts.com">Laracasts</a>
-        <a href="https://laravel-news.com">News</a>
-        <a href="https://forge.laravel.com">Forge</a>
-        <a href="https://github.com/laravel/laravel">GitHub</a>
+        <a href="https://github.com/cretueusebiu/laravel-vue-spa">github.com/cretueusebiu/laravel-vue-spa</a>
       </div>
     </div>
   </div>
