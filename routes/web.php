bd9e67be1372aea59433f91cc18a3ca037df2f16
Hrvoje Jukic <hrcajuka@gmail.com>
2020-04-09 12:12:48 +0200

    Moved SPA endpoint to controller.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{path}', function () {
-    return view('index');
-})->where('path', '(.*)');
+Route::get('{path}', 'SpaController@index')->where('path', '(.*)');
