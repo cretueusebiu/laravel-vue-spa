408bd8ed41f5f48f865cee5f622c5d0fd6617484
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2020-10-31 13:48:03 +0200

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('spa')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/spa.php'));
         });
     }
 
