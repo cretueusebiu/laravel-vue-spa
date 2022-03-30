417a043cf1abfaeb2f577febd0287d8be375d7a0
Ahmad Ishaq <ahmadishaqcs@gmail.com>
2022-03-30 09:55:39 +0500

    col 2

--- resources/js/pages/customers/CustomerDetail.vue
+++ resources/js/pages/customers/CustomerDetail.vue
@@ -93,32 +93,51 @@
         </v-card-text>
       </v-card>
     </v-col>
-    <v-col lg="8" md="7"> 
+    <v-col lg="8" md="7">
       <v-card>
-    <v-tabs
-      v-model="tab"
-      background-color="primary"
-      dark
-    >
-      <v-tab
-        v-for="item in items"
-        :key="item.tab"
-      >
-        {{ item.tab }}
-      </v-tab>
-    </v-tabs>
+        <v-tabs
+          centered
+          color="deep-purple accent-4 show-arrows"
+          next-icon="mdi-arrow-right-bold-box-outline"
+          prev-icon="mdi-arrow-left-bold-box-outline"
+        >
+          <v-tab>Landscape</v-tab>
+          <v-tab>City</v-tab>
+          <v-tab>Abstract</v-tab>
 
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-        v-for="item in items"
-        :key="item.tab"
-      >
-        <v-card flat>
-          <v-card-text>{{ item.content }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
+          <v-tab-item v-for="n in 3" :key="n">
+            <v-container fluid>
+              <v-col>
+                <v-col v-for="i in 3" :key="i" >
+                  <!-- <v-img
+                    :src="`https://picsum.photos/500/300?image=${
+                      i * n * 5 + 10
+                    }`"
+                    :lazy-src="`https://picsum.photos/10/6?image=${
+                      i * n * 5 + 10
+                    }`"
+                    aspect-ratio="1"
+                  ></v-img> -->
+                  <v-card class="mx-auto"  outlined>
+                    <v-list-item three-line>
+                      <v-list-item-content>
+                        <div class="text-overline mb-4">OVERLINE</div>
+                        <v-list-item-title class="text-h5 mb-1">
+                          Headline 5
+                        </v-list-item-title>
+                        <v-list-item-subtitle
+                          >Greyhound divisely hello coldly
+                          fonwderfully</v-list-item-subtitle
+                        >
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-card>
+                </v-col>
+              </v-col>
+            </v-container>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -127,23 +146,22 @@
 export default {
   middleware: "auth",
 
-  data () {
-      return {
-        tab: null,
-        items: [
-          { tab: 'One', content: 'Tab 1 Content' },
-          { tab: 'Two', content: 'Tab 2 Content' },
-          { tab: 'Three', content: 'Tab 3 Content' },
-          { tab: 'Four', content: 'Tab 4 Content' },
-          { tab: 'Five', content: 'Tab 5 Content' },
-          { tab: 'Six', content: 'Tab 6 Content' },
-          { tab: 'Seven', content: 'Tab 7 Content' },
-          { tab: 'Eight', content: 'Tab 8 Content' },
-          { tab: 'Nine', content: 'Tab 9 Content' },
-          { tab: 'Ten', content: 'Tab 10 Content' },
-        ],
-      }
-    },
+  // data () {
+  //     return {
+  //       tab: null,
+  //       items: [
+  //         { tab: 'One', content: 'Tab 1 Content' },
+  //         { tab: 'Two', content: 'Tab 2 Content' },
+  //         { tab: 'Three', content: 'Tab 3 Content' },
+  //         { tab: 'Four', content: 'Tab 4 Content' },
+  //         { tab: 'Five', content: 'Tab 5 Content' },
+  //         { tab: 'Six', content: 'Tab 6 Content' },
+  //         { tab: 'Seven', content: 'Tab 7 Content' },
+  //         { tab: 'Eight', content: 'Tab 8 Content' },
+  //         { tab: 'Nine', content: 'Tab 9 Content' },
+  //         { tab: 'Ten', content: 'Tab 10 Content' },
+  //       ],
+  //     }
+  //   },
 };
-
 </script>
