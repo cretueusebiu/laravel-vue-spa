6922db05f082dae4d7819f3f97d88be194774921
Ahmad Ishaq <ahmadishaqcs@gmail.com>
2022-04-04 11:49:40 +0500

    account_setting_design
    completed

--- resources/js/pages/customers/AccountSetting.vue
+++ resources/js/pages/customers/AccountSetting.vue
@@ -1,4 +1,4 @@
-<!--<template>
+<template>
   <v-card>
     <v-tabs>
       <v-tab>
@@ -12,18 +12,21 @@
             role="img"
             aria-hidden="true"
             class="v-icon__svg"
-            style="font-size: 20px; height: 20px; width: 20px">
+            style="font-size: 20px; height: 20px; width: 20px"
+          >
             <path
               d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,6A2,2 0 0,0 10,8A2,2 0 0,0 12,10A2,2 0 0,0 14,8A2,2 0 0,0 12,6M12,13C14.67,13 20,14.33 20,17V20H4V17C4,14.33 9.33,13 12,13M12,14.9C9.03,14.9 5.9,16.36 5.9,17V18.1H18.1V17C18.1,16.36 14.97,14.9 12,14.9Z"
-            ></path></svg></span>Account</v-tab
+            ></path></svg></span
+        >Account</v-tab
       >
       <v-tab>Item Two</v-tab>
       <v-tab>Item Three</v-tab>
     </v-tabs>
-    <v-card-text  class="d-flex pa-10 mt-2">
+    <v-card-text class="d-flex pa-10 mt-2">
       <div
         class="v-avatar me-6 rounded"
-        style="height: 120px; min-width: 120px; width: 120px">
+        style="height: 120px; min-width: 120px; width: 120px"
+      >
         <v-img>
           <div
             class="v-image__image v-image__image--cover"
@@ -42,77 +45,97 @@
         <p class="text-sm mt-5">Allowed JPG, GIF or PNG. Max size of 800K</p>
       </div>
     </v-card-text>
-    <v-card-text class="d-flex pa-10 mt-1">
+    <v-card-text>
       <v-form>
-        <div class="row">
-        
-          <v-text-field
-            class="col-md-6 col-12"
-            v-model="name"
-            :error-messages="nameErrors"
-            placeholder="Name"
-            outlined
-            dense
-            label="Name"
-            required
-          ></v-text-field>
-          <v-text-field
-            class="col-md-6 col-12"
-            v-model="name"
-            :error-messages="nameErrors"
-            label="User Name"
-            dense
-            outlined
-            required
-          ></v-text-field>
-          <v-text-field
-            class="col-md-5 col-12 mr-2"
-            v-model="name"
-            dense
-            :error-messages="nameErrors"
-            outlined
-            label="Email"
-            required
-          ></v-text-field>
-          <v-text-field
-            class="col-md-5 col-12"
-            v-model="name"
-            :error-messages="nameErrors"
-            outlined
-            dense
-            label="Admin"
-            required
-          ></v-text-field>
-          <v-select
-            class="col-md-5 col-12 mr-2"
-            v-model="select"
-            outlined
-            :items="items"
-            dense
-            :error-messages="selectErrors"
-            label="Item"
-            required
-          ></v-select>
-           <v-text-field
-            class="col-md-5 col-12"
-            v-model="name"
-            outlined
-            dense
-            :error-messages="nameErrors"
-            label="Company"
-            required
-          ></v-text-field>
-          <div cٖlass="col-md-6 col-12 pa-10 mt-2">
-            <v-btn class="me-3 mt-4 v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default primary" @click="submit"> submit </v-btn>
-            <v-btn class="mt-4 v-btn v-btn--outlined theme--light v-size--default secondary--text" @click="clear"> clear </v-btn>
-          </div>
-        </div>
+        <v-row>
+          <v-col class="col-md-6 col-12">
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              placeholder="SaadKamran"
+              label="User Name"
+              outlined
+              dense
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col class="col-md-6-12">
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              placeholder="Saad Kamran"
+              label="Name"
+              outlined
+              dense
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col class="col-md-6 col-12">
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              placeholder="Saad Kamran"
+              label="Email"
+              outlined
+              dense
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col class="col-md-6 col-12">
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              placeholder="Admin"
+              label="Role"
+              dense
+              outlined
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col class="col-md-6 col-12">
+            <v-select
+              v-model="name"
+              :error-messages="nameErrors"
+              placeholder="Active"
+              label="Status"
+              outlined
+              dense
+              required
+            ></v-select>
+          </v-col>
+          <v-col class="col-md-6 col-12">
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              placeholder="Google,inc"
+              label="Company"
+              dense
+              outlined
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col class="col col-12">
+            <v-alert class="pa-4" dense text icon="mdi-alert" type="warning">
+              <strong
+                >Your email is not confirmed. Please check your inbox.</strong
+              >
+              <br />
+              Resend Confirmation
+            </v-alert>
+          </v-col>
+          <v-col class="col col-12">
+            <v-btn class="me-3 mt-4 theme--light primary">
+              Save Changes
+            </v-btn>
+            <v-btn class="mt-4  theme--light" color="grey" outlined>
+              Cancel
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-form>
     </v-card-text>
   </v-card>
-</template> -->
-
-<!-- Billing template -->
+</template>
 <script>
 export default {};
 </script>
