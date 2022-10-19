868a7918b1921ad5d3bcdf9c7a83d1d8b0771b7e
shafqat <shafqatali89@web.de>
2022-10-19 23:23:58 +0200

    Trusted Proxies Bug Fixed

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 // use Fideloper\Proxy\TrustProxies as Middleware;
-use Illuminate\Http\Middleware\TrustProxies as Middleware
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
